a53a1f0bea129bb893d0bfa46cba168e2809c6c8
guangyaoz <thzheng7@gmail.com>
2020-04-02 16:48:04 -0400

    change questions to separate activities. Add start, login, sign up activities. Add gridview to home fragment

---
+++ app/src/main/java/com/example/nestly/HabitsActivity.java
@@ -0,0 +1,14 @@
+package com.example.nestly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HabitsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habits);
+    }
+}
