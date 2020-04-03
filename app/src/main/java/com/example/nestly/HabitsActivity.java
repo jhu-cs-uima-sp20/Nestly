40f9c4a2ce9856020c47a6fd5ee95f790b280385
Max Torres <maxtorres487@gmail.com>
2020-04-03 13:59:15 -0400

    instantiated database ref in every activity, took buttons out of MainActivity, added gradle implementations

--- app/src/main/java/com/example/nestly/HabitsActivity.java
+++ app/src/main/java/com/example/nestly/HabitsActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HabitsActivity extends AppCompatActivity {
+
+    private FirebaseDatabase myBase;
+    private DatabaseReference dbref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habits);
+
+        myBase = FirebaseDatabase.getInstance();
+        dbref = myBase.getReference();
     }
 }
--- app/src/main/java/com/example/nestly/LoginActivity.java
+++ app/src/main/java/com/example/nestly/LoginActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseDatabase myBase;
+    private DatabaseReference dbref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        myBase = FirebaseDatabase.getInstance();
+        dbref = myBase.getReference();
     }
 }
