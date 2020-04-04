97573d2b9ab1355b262ef470921711a4fdbf9f5b
Emily Zeng <ezeng2@jhu.edu>
2020-04-03 23:46:25 -0600

    added buttons and images to start, signup, and login screens. added basic error handling to login and signup (when user puts in non-jhu email or leaves password empty). linked activities together so when you click buttons from start activity, the corresponding login and signup activities will be launched

--- app/src/main/java/com/example/nestly/SignupActivity.java
+++ app/src/main/java/com/example/nestly/SignupActivity.java
@@ -2,15 +2,15 @@ package com.example.nestly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,9 +19,17 @@ public class SignupActivity extends AppCompatActivity implements AdapterView.OnI
     private TextView name;
     private TextView email;
     private TextView password;
-    private ImageButton signup;
+    private Button signup;
+    private Spinner major;
     private Spinner year;
     private Spinner gender;
+    private ArrayAdapter<CharSequence> adapter1;
+    private ArrayAdapter<CharSequence> adapter2;
+    private ArrayAdapter<CharSequence> adapter3;
+
+    private boolean checkedYear;
+    private boolean checkedMajor;
+    private boolean checkedGender;
 
     private FirebaseDatabase myBase;
     private DatabaseReference dbref;
@@ -35,30 +43,86 @@ public class SignupActivity extends AppCompatActivity implements AdapterView.OnI
         name = (TextView) findViewById(R.id.name_txt);
         email = (TextView) findViewById(R.id.email);
         password = (TextView) findViewById(R.id.password);
-        signup = (ImageButton) findViewById(R.id.signup_btn);
+        signup = (Button) findViewById(R.id.signup_btn);
+        major = (Spinner) findViewById(R.id.major);
         year = (Spinner) findViewById(R.id.year);
         gender = (Spinner) findViewById(R.id.gender);
 
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!checkValid())
+                    return;
+
+                // move to habits portion of the quiz
+                Intent habits_intent = new Intent(getApplicationContext(), HabitsActivity.class);
+                startActivity(habits_intent);
+            }
+        });
+
         //set up year spinner
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+        adapter1 = ArrayAdapter.createFromResource(this,
                 R.array.year_options, android.R.layout.simple_spinner_dropdown_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         year.setAdapter(adapter1);
+        year.setPrompt("Year");
 
         //set up gender spinner
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+        adapter2 = ArrayAdapter.createFromResource(this,
                 R.array.gender_options, android.R.layout.simple_spinner_dropdown_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gender.setAdapter(adapter2);
 
+        // set up major spinner
+        adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.major_options, android.R.layout.simple_spinner_dropdown_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        major.setAdapter(adapter3);
+
+        // database
         myBase = FirebaseDatabase.getInstance();
         dbref = myBase.getReference();
 
     }
 
+    /*
+     * Checks if the information inputted is valid
+     * @return true if valid, false otherwise
+     */
+    public boolean checkValid() {
+        // if the user puts in an incorrect JHU email
+        String jhu_email = email.getText().toString();
+        String pass = password.getText().toString();
+        if(jhu_email.indexOf("@jh.edu") < 0) {
+            Toast.makeText(getBaseContext(),
+                    "Invalid Email!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (pass.length() == 0) {
+            Toast.makeText(getBaseContext(),
+                    "Please Enter Password!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!checkedMajor || !checkedYear || !checkedGender) {
+            Toast.makeText(getBaseContext(),
+                    "Fields Missing!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
+
+        // checks if field was selected
+        if (parent.equals(adapter1)) {
+            checkedYear = true;
+        } else if (parent.equals(adapter2)) {
+            checkedGender = true;
+        } else if (parent.equals(adapter3)) {
+            checkedMajor = true;
+        }
+
+        // add sharing preferences here
     }
 
     @Override
--- app/src/main/java/com/example/nestly/StartActivity.java
+++ app/src/main/java/com/example/nestly/StartActivity.java
@@ -2,7 +2,10 @@ package com.example.nestly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,6 +14,8 @@ public class StartActivity extends AppCompatActivity {
 
     private FirebaseDatabase myBase;
     private DatabaseReference dbref;
+    private Button login;
+    private Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,24 @@ public class StartActivity extends AppCompatActivity {
 
         myBase = FirebaseDatabase.getInstance();
         dbref = myBase.getReference();
+
+        login = (Button) findViewById(R.id.login_button);
+        signup = (Button) findViewById(R.id.signup_button);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup_intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(signup_intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login_intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(login_intent);
+            }
+        });
     }
 }
