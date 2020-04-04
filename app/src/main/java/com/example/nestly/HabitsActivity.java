ac9d8eff89cdf023872697059fada5388fa62516
guangyaoz <thzheng7@gmail.com>
2020-04-04 19:48:54 -0400

    added rough design implementation to habits activity

--- app/src/main/java/com/example/nestly/HabitsActivity.java
+++ app/src/main/java/com/example/nestly/HabitsActivity.java
@@ -3,12 +3,22 @@ package com.example.nestly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class HabitsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setTitle("Habits");
         setContentView(R.layout.activity_habits);
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.identify_as, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/nestly/MainActivity.java
+++ app/src/main/java/com/example/nestly/MainActivity.java
@@ -14,18 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginButton = (Button) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToLogin();
-            }
-        });
     }
 
     public void goToLogin() {
         startActivity(new Intent(this,
-                com.example.nestly.ui.login.LoginActivity.class));
+                com.example.nestly.LoginActivity.class));
     }
 }
 
