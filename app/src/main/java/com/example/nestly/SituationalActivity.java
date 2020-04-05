d7387a815927e43d7c5ca1dda322e495c85e20e7
guangyaoz <thzheng7@gmail.com>
2020-04-04 20:07:00 -0400

    added rough design implementation for situational questions and long answers

--- app/src/main/java/com/example/nestly/SituationalActivity.java
+++ app/src/main/java/com/example/nestly/SituationalActivity.java
@@ -10,5 +10,6 @@ public class SituationalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_situational);
+        getActionBar().setTitle("Situations");
     }
 }
