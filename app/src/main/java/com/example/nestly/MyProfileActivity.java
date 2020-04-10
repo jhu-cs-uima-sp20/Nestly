1a9dd3e46fb65351feb738d909e29aab3f5bcdab
zz328 <emilyzeng328@gmail.com>
2020-04-09 22:56:27 -0600

    added menu to profile to give option to edit bio

--- app/src/main/java/com/example/nestly/MyProfileActivity.java
+++ app/src/main/java/com/example/nestly/MyProfileActivity.java
@@ -61,6 +61,10 @@ public class MyProfileActivity extends AppCompatActivity implements View.OnClick
         String year = myPrefs.getString("year", "2022");
         String major = myPrefs.getString("major", "Undeclared");
 
+        my_name.setText(name);
+        my_major.setText(major);
+        my_year.setText("Class Of " + year);
+
         uploadPic.setOnClickListener(this);
 
         TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
@@ -108,5 +112,29 @@ public class MyProfileActivity extends AppCompatActivity implements View.OnClick
                 break;
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.edit_bio) {
+            Toast.makeText(getBaseContext(),
+                    "User Blocked!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
