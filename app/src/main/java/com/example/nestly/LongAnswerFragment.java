3ae494d0bac0f24e6ce2aaf4cd141fc5aa7278b2
Jennifer <jehhiy@gmail.com>
2020-04-10 19:05:56 -0400

    small

--- app/src/main/java/com/example/nestly/LongAnswerFragment.java
+++ app/src/main/java/com/example/nestly/LongAnswerFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,6 +24,11 @@ public class LongAnswerFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private TextView typicalDay;
+    private TextView worstHabit;
+    private TextView hopeRoommate;
+    private TextView other;
 
     public LongAnswerFragment() {
         // Required empty public constructor
@@ -59,6 +65,13 @@ public class LongAnswerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_long_answer, container, false);
+        View v = inflater.inflate(R.layout.fragment_long_answer, container, false);
+
+        typicalDay = v.findViewById(R.id.typicalDay);
+        worstHabit = v.findViewById(R.id.worstHabit);
+        hopeRoommate = v.findViewById(R.id.hopeRoommate);
+        other = v.findViewById(R.id.other);
+
+        return v;
     }
 }
